0df030fdcd65a33c709c11e95f122756352340da
Meyer McMains <meyer.mcmains@gmail.com>
2019-11-29 08:15:20 -0600

    WIP on updating / retrieve library triggers

--- src/screens/Music/index.js
+++ src/screens/Music/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import Container from '@components/Container';
-import Svg from '@components/Svg';
+import Grid from './components/Grid';
+// import Svg from '@components/Svg';
 
-const Music = () => (
-  <Container>
-    <Svg />
-  </Container>
-);
+// import { useAsyncEffect } from '@hooks';
+import { actions } from '@modules/library';
+import { screenPropsType } from '@types';
 
-export default Music;
+const Music = ({
+  getLibrary,
+  // isPending,
+  library,
+  screenProps
+  // updateLibrary
+}) => {
+  // const [result] = useAsyncEffect(updateLibrary);
+  // const [result] = useAsyncEffect(getLibrary);
+  const albums =
+    library.length > 1 &&
+    library.map(artist => artist.albums).reduce((acc, cur) => [...acc, ...cur]);
+
+  return (
+    <>
+      {/* {typeof result !== 'boolean' && <Svg theme={screenProps.theme} />} */}
+      {/* <Svg theme={screenProps.theme} /> */}
+      <Container>
+        <Grid data={albums} />
+      </Container>
+    </>
+  );
+};
+
+Music.propTypes = {
+  getLibrary: PropTypes.func.isRequired,
+  library: PropTypes.array.isRequired,
+  screenProps: screenPropsType
+};
+
+const mapStateToProps = ({ library }) => ({
+  isPending: library.isPending,
+  library: library.data
+});
+
+export default connect(mapStateToProps, {
+  getLibrary: actions.getLibrary,
+  updateLibrary: actions.updateLibrary
+})(Music);
