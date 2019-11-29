47dba2665264a7ed241d8580c785836515fb448b
Meyer McMains <meyer.mcmains@gmail.com>
2019-11-29 08:09:55 -0600

    add more types

--- src/types.js
+++ src/types.js
@@ -8,3 +8,30 @@ export const screenPropsType = PropTypes.shape({
   theme: themeType,
   updateTheme: PropTypes.func.isRequired
 }).isRequired;
+
+export const trackType = PropTypes.shape({
+  length: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.number.isRequired
+}).isRequired;
+
+export const artworkType = PropTypes.shape({
+  color: PropTypes.string.isRequired,
+  file: PropTypes.string.isRequired,
+  isDark: PropTypes.bool
+}).isRequired;
+
+export const albumType = PropTypes.shape({
+  artist: PropTypes.string.isRequired,
+  artwork: artworkType,
+  title: PropTypes.string.isRequired,
+  tracks: PropTypes.arrayOf(trackType),
+  year: PropTypes.number
+}).isRequired;
+
+export const libraryType = PropTypes.arrayOf(
+  PropTypes.shape({
+    albums: PropTypes.arrayOf(albumType),
+    artist: PropTypes.string.isRequired
+  }).isRequired
+).isRequired;
