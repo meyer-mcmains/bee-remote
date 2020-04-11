f491e717b811435b72d96c24446aaef7edfea58a
Meyer McMains <meyer.mcmains@gmail.com>
2020-04-11 16:12:37 -0500

    make separate container for web

--- src/components/Container/index.web.js
+++ src/components/Container/index.web.js
@@ -2,40 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { withTheme } from 'styled-components/native';
 
-import Header from '../Header';
-
 import { themeType } from '@types';
-
-const SafeAreaView = styled.SafeAreaView`
-  flex: 1;
-`;
 
 const Wrapper = styled.View`
   background-color: ${p => p.theme.color.primary};
   bottom: 0;
   flex: 1;
-  height: 100%;
+  height: 100vh;
   left: 0;
-  padding-left: 16px;
-  padding-right: 16px;
   position: absolute;
   right: 0;
   top: 0;
 `;
 
-const Container = ({ children, title, theme }) => (
-  <>
-    <Header main title={title} />
-    <Wrapper>
-      <SafeAreaView>{children}</SafeAreaView>
-    </Wrapper>
-  </>
-);
+const Container = ({ children, theme }) => <Wrapper>{children}</Wrapper>;
 
 Container.propTypes = {
   children: PropTypes.node.isRequired,
-  theme: themeType,
-  title: PropTypes.string
+  theme: themeType
 };
 
 export default withTheme(Container);
