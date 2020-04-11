76440c436e142469805a5aef7eebd28a5927308f
Meyer McMains <meyer.mcmains@gmail.com>
2020-04-11 16:50:19 -0500

    add more types

--- src/types.js
+++ src/types.js
@@ -12,7 +12,8 @@ export const screenPropsType = PropTypes.shape({
 export const trackType = PropTypes.shape({
   length: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  number: PropTypes.number.isRequired
+  number: PropTypes.number.isRequired,
+  trackID: PropTypes.string.isRequired
 }).isRequired;
 
 export const artworkType = PropTypes.shape({
@@ -22,11 +23,12 @@ export const artworkType = PropTypes.shape({
 }).isRequired;
 
 export const albumType = PropTypes.shape({
+  albumID: PropTypes.string.isRequired,
   artist: PropTypes.string.isRequired,
   artwork: artworkType,
   title: PropTypes.string.isRequired,
   tracks: PropTypes.arrayOf(trackType),
-  year: PropTypes.number
+  year: PropTypes.string
 }).isRequired;
 
 export const libraryType = PropTypes.arrayOf(
@@ -35,3 +37,8 @@ export const libraryType = PropTypes.arrayOf(
     artist: PropTypes.string.isRequired
   }).isRequired
 ).isRequired;
+
+export const styleType = PropTypes.oneOfType([
+  PropTypes.array,
+  PropTypes.object
+]);
